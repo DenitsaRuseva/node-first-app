0258a217204dd939fb65b73edb3a2fcf389b5a7e
DenicaRuseva <drr_87@abv.bg>
2019-09-27 17:32:54 +0100

    path.js

--- routs/admin.js
+++ routs/admin.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 
+const rootPath = require('../utill/path');
+
+
 
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootPath, 'views', 'add-product.html'));
 });
 
 
--- routs/shop.js
+++ routs/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootPath = require('../utill/path');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootPath, 'views', 'shop.html'));
 });
 
 module.exports = router;
