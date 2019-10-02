f6ebdf944b7e7ce742e4a9b7545223f2184897c6
DenicaRuseva <drr_87@abv.bg>
2019-10-02 12:31:56 +0100

    extracting product id

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,6 +11,14 @@ exports.getProducts = (req, res, next) => {
     });
   };
 
+  exports.getProduct = (req, res) => {
+    const productId = req.params.productId;
+    Product.findProduct(productId, product => {
+      console.log(product);
+      res.redirect('/');
+    })
+  }
+
   exports.getIndex = (req, res, next) => {
     Product.fetchAll(products => {
       res.render('shop/index', {
